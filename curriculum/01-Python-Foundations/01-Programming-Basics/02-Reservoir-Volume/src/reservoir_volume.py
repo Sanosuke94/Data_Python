a992b1b4eff5fda5b5d5852b45123d1340f514d8
Sanosuke <cedric-valcy@hotmail.fr>
2019-04-02 12:32:32 +0200

    Exercice 2

--- curriculum/01-Python-Foundations/01-Programming-Basics/02-Reservoir-Volume/src/reservoir_volume.py
+++ curriculum/01-Python-Foundations/01-Programming-Basics/02-Reservoir-Volume/src/reservoir_volume.py
@@ -12,18 +12,19 @@ def main():
     rainfall = 5e6
 
     # TODO: decrease the rainfall variable by 10% to account for runoff
-
+    rainfall = rainfall * 0.9
     # TODO: add the rainfall variable to the reservoir_volume variable
-
+    reservoir_volume = rainfall + reservoir_volume
     # TODO: increase reservoir_volume by 5% to account for stormwater that
     # flows into the reservoir in the days following the storm
-
+    reservoir_volume = reservoir_volume * 1.05
     # TODO: decrease reservoir_volume by 5% to account for evaporation
-
+    reservoir_volume = reservoir_volume * 0.95
     # TODO: subtract 2.5e5 cubic metres from reservoir_volume to account for
     # water that's piped to arid regions.
-
+    reservoir_volume = reservoir_volume - 2.5e5
     # TODO: print the new value of the reservoir_volume variable
+    print(reservoir_volume)
 
 
 if __name__ == '__main__':
