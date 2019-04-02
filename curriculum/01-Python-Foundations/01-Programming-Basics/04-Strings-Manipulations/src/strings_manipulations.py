b38f00448f8c7a61977e5257b05d312cdbc4027a
Sanosuke <cedric-valcy@hotmail.fr>
2019-04-02 13:17:47 +0200

    Exercice 4

--- curriculum/01-Python-Foundations/01-Programming-Basics/04-Strings-Manipulations/src/strings_manipulations.py
+++ curriculum/01-Python-Foundations/01-Programming-Basics/04-Strings-Manipulations/src/strings_manipulations.py
@@ -9,9 +9,13 @@ def main():
     ## Part 1: using string methods, print out what you're being asked
     # TODO: print the lowercase version of this string
     up_string = "A string with UPPERCASE"
+    print(up_string.lower())
+    #print(up_string.upper())
 
     # TODO: print the length of this string
     long_string = "A very very long string, don't you think?"
+
+    print(len(long_string))
 
     ## Part 2
     username = "Kinari"
@@ -21,11 +25,16 @@ def main():
     # TODO: print a log message using the variables above.
     # The message should have the same format as this one:
     # "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+    print("%s accessed the site %s at %s." %( username, url, timestamp))
 
 
     ## Part 3
     maria_string = "Maria loves {} and {}."
-    # TODO: print out "Maria loves math and statistics." using .format()
+    var_1 = "math"
+    var_2 = "statistics"
+    print(maria_string.format(var_1, var_2)) # TODO: print out "Maria loves math and statistics." using .format()
+
+    
 
 
 if __name__ == '__main__':
